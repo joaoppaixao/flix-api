5f98787f53d85285a973487632fcd0f07a2f50e6
joao <dev@monitoo.com.br>
2026-03-24 10:26:52 -0300

    adicionando o metodo DELETE

--- genres/views.py
+++ genres/views.py
@@ -37,3 +37,10 @@ def genre_detail_view(request, pk):
         return JsonResponse(
             {'id': genre.id, 'name': genre.name}
         )
+
+    elif request.method == 'DELETE':
+        genre.delete()
+        return JsonResponse(
+            {'message': 'Gênero excluído com sucesso!'},
+            status=204,
+        )

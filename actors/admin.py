f48c9363b73930536f0c5e5c761d527b35677d8b
joao <dev@monitoo.com.br>
2026-03-25 11:16:32 -0300

    criando CRUD, view e url de actors

--- actors/admin.py
+++ actors/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from actors.models import Actor
 
-# Register your models here.
+
+@admin.register(Actor)
+class ActorsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'birthday', 'nationality')
+    search_fields = ('name', 'nationality')
+

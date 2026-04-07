70f63be5a2398a942af739dc35af5cc36083ed0f
joao <dev@monitoo.com.br>
2026-04-07 10:50:25 -0300

    feat: implementando o arquivo permissions para permissoes especificas

---
+++ genres/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework import permissions 
+
+
+class GenrePermissionClass(permissions.BasePermission):
+    
+    def has_permission(self, request, view):
+        # logica da permissao
+        return True

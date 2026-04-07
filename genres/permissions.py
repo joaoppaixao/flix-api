9222e010f84a0c1c15b60c5d8d5af080a6ea74cf
joao <dev@monitoo.com.br>
2026-04-07 12:18:29 -0300

    fix: adicionando o metodo PATCH junto com o PUT

--- genres/permissions.py
+++ genres/permissions.py
@@ -10,7 +10,7 @@ class GenrePermissionClass(permissions.BasePermission):
         if request.method == 'POST':
             return request.user.has_perm('genres.add_genre')
         
-        if request.method == 'PUT':
+        if request.method in ['PATCH', 'PUT']:
             return request.user.has_perm('genres.change_genre')
         
         if request.method == 'DELETE':
